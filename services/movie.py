30edf1de3a7d951a163c07e90ec06c0964ed3525
Ivan Kozerovkiy <30504180+vanko6226@users.noreply.github.com>
2024-05-03 22:13:54 +0300

    Update services/movie.py
    Co-authored-by: Eduard Alekseiev <edikaleks43+github@gmail.com>

--- services/movie.py
+++ services/movie.py
@@ -24,8 +24,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 
 def create_movie(movie_title: str,
                  movie_description: str,
-                 genres_ids: list = None,
-                 actors_ids: list = None) -> Movie:
+                 genres_ids: list[int] | None = None,
+                 actors_ids: list[int] | None = None) -> Movie:
     movie = Movie.objects.create(title=movie_title,
                                  description=movie_description,)
 
