fd23ea268c17812f02c1248ac6beda1ddcb2de95
Ivan Kozerovkiy <30504180+vanko6226@users.noreply.github.com>
2024-05-03 22:09:30 +0300

    Update services/cinema_hall.py
    Co-authored-by: Eduard Alekseiev <edikaleks43+github@gmail.com>

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -3,7 +3,7 @@ from django.db.models import QuerySet
 from db.models import CinemaHall
 
 
-def get_cinema_halls() -> QuerySet:
+def get_cinema_halls() -> QuerySet[CinemaHall]:
     return CinemaHall.objects.all()
 
 
